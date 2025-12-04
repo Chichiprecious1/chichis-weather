ab1014b0c9ea084ce4701806e163361f1671bac4
Chichiprecious1 <mulibanatshilidzi11@gmail.com>
2025-12-03 20:36:32 -0500

    added links

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import React from "react";
+
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.shecodes.io/graduates/92144-tshilidzi-mulibana"
-          target="_blank"
-        >
-          {" "}
-          Tshilidzi Mulibana{" "}
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a
-          href="httphttps://github.com/Chichiprecious1/chichis-weathers://github.com/Chichiprecious1"
-          target="_blank"
-        >
-          {" "}
-          Github
-        </a>
-      </footer>
+      <div className="container">
+        <footer>
+          This project was coded by {""}
+          <a
+            href=" https://www.shecodes.io/graduates/92144-tshilidzi-mulibana"
+            target="_blank"
+          >
+            Tshilidzi Mulibana
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/Chichiprecious1/chichis-weather/blob/main/src/App.js"
+            target="_blank"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a href="https://chichis-weather.netlify.app/" target="_blank">
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
