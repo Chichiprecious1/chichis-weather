3927612c82722cba48c39fb5a429a3f40e686b6b
Chichiprecious1 <mulibanatshilidzi11@gmail.com>
2025-12-03 20:44:44 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,27 +5,30 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
       <div className="container">
+        <h1>Weather App</h1>
         <footer>
-          This project was coded by {""}
+          This project was coded by{" "}
           <a
-            href=" https://www.shecodes.io/graduates/92144-tshilidzi-mulibana"
+            href="https://www.shecodes.io/..."
             target="_blank"
+            rel="noreferrer"
           >
             Tshilidzi Mulibana
           </a>{" "}
           and is open-sourced on{" "}
-          <a
-            href="https://github.com/Chichiprecious1/chichis-weather/blob/main/src/App.js"
-            target="_blank"
-          >
+          <a href="https://github.com/..." target="_blank" rel="noreferrer">
             GitHub
           </a>{" "}
           and hosted on{" "}
-          <a href="https://chichis-weather.netlify.app/" target="_blank">
+          <a
+            href="https://chichis-weather.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
             Netlify
           </a>
+          .
         </footer>
       </div>
     </div>
