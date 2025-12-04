78b4b70988a07ad2ed79d283f5b1f6e40e0e668d
Chichiprecious1 <mulibanatshilidzi11@gmail.com>
2025-12-03 20:12:09 -0500

    added the footer

--- src/App.js
+++ src/App.js
@@ -4,7 +4,25 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.shecodes.io/graduates/92144-tshilidzi-mulibana"
+          target="_blank"
+        >
+          {" "}
+          Tshilidzi Mulibana{" "}
+        </a>{" "}
+        and is open-sourced on{" "}
+        <a
+          href="httphttps://github.com/Chichiprecious1/chichis-weathers://github.com/Chichiprecious1"
+          target="_blank"
+        >
+          {" "}
+          Github
+        </a>
+      </footer>
     </div>
   );
 }
