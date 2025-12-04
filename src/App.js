4b0e0fcd7f1195ab2eab951ccd06a3b01201b1fe
Chichiprecious1 <mulibanatshilidzi11@gmail.com>
2025-12-03 20:19:20 -0500

    .

--- src/App.js
+++ src/App.js
@@ -5,24 +5,6 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
-      <p>
-        This project was coded by{" "}
-        <a
-          href="https://www.shecodes.io/graduates/92144-tshilidzi-mulibana"
-          target="_blank"
-        >
-          {" "}
-          Tshilidzi Mulibana{" "}
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a
-          href="httphttps://github.com/Chichiprecious1/chichis-weathers://github.com/Chichiprecious1"
-          target="_blank"
-        >
-          {" "}
-          Github
-        </a>
-      </p>
     </div>
   );
 }
