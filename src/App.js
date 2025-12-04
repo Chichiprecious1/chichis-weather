1289acd4964afc46bddf26d38df7d0654140b2aa
Chichiprecious1 <mulibanatshilidzi11@gmail.com>
2025-12-03 20:10:32 -0500

    Checking if the repository works on netlify

--- src/App.js
+++ src/App.js
@@ -4,25 +4,7 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.shecodes.io/graduates/92144-tshilidzi-mulibana"
-          target="_blank"
-        >
-          {" "}
-          Tshilidzi Mulibana{" "}
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a
-          href="httphttps://github.com/Chichiprecious1/chichis-weathers://github.com/Chichiprecious1"
-          target="_blank"
-        >
-          {" "}
-          Github
-        </a>
-      </footer>
+      <h1>Hello world</h1>
     </div>
   );
 }
