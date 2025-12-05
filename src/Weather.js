762dde7de92f7f5b96459deba4d7f6ccd5a7e44e
Chichiprecious1 <mulibanatshilidzi11@gmail.com>
2025-12-04 22:19:29 -0500

    added form and images

--- src/Weather.js
+++ src/Weather.js
@@ -2,5 +2,44 @@ import React from "react";
 import "./Weather.css";
 
 export default function Weather() {
-  return <div className="Weather">Hello From Weather</div>;
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Mostly cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://img.icons8.com/emoji/1200/sun-behind-cloud.jpg"
+            alt="Partly cloudy weather icon"
+            style={{ width: "100px", height: "auto" }}
+          />
+          6℃
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Percipitation: 15%</li>
+            <li>Humidity:72% </li>
+            <li>Wind: 13km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
